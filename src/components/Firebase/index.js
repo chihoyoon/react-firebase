f271110931b32d2bec2ec60ccb254a36c808e8d9
chihoyoon <chiho.yoon@hotmail.com>
2021-09-30 22:45:00 -0600

    setup components

---
+++ src/components/Firebase/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Firebase extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Firebase</h1>
+            </div>
+        );
+    }
+}
+
+export default Firebase;
