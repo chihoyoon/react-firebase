636cf722b62354bf74919e5cbfddc274b9549698
chihoyoon <chiho.yoon@hotmail.com>
2021-09-30 23:31:14 -0600

    setup router

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router} from 'react-router-dom';
 
+import Navigation from './Navigation';
 class App extends Component {
     render() {
         return (
-            <div>
-                <h1>App</h1>
-            </div>
+            <Router>
+                <Navigation />
+            </Router>
         );
     }
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,28 @@
 import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 
+import * as ROUTES from '../constants/routes';
 class Navigation extends Component {
     render() {
         return (
             <div>
-                <h1>Navigation</h1>
+                <ul>
+                    <li>
+                        <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+                    </li>
+                    <li>
+                        <Link to={ROUTES.LANDING}>Landing</Link>
+                    </li>
+                    <li>
+                        <Link to={ROUTES.HOME}>Home</Link>
+                    </li>
+                    <li>
+                        <Link to={ROUTES.ACCOUNT}>Account</Link>
+                    </li>
+                    <li>
+                        <Link to={ROUTES.ADMIN}>Admin</Link>
+                    </li>
+                </ul>
             </div>
         );
     }
