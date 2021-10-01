7027412ad4f50c83824ef4ad3876957c2aaf240d
chihoyoon <chiho.yoon@hotmail.com>
2021-10-01 01:28:43 -0600

    import firebase

--- src/components/firebase/index.js
+++ src/components/firebase/index.js
@@ -1,13 +1,6 @@
-import React, { Component } from 'react';
+import FirebaseContext from './context';
+import Firebase from './firebase';
 
-class Firebase extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Firebase</h1>
-            </div>
-        );
-    }
-}
 
 export default Firebase;
+export { FirebaseContext };
